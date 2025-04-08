b3c8333770618b7e40c32458b374f756f3894f21
Duy Le <20849323+Groawp@users.noreply.github.com>
2025-04-07 21:36:16 -0700

    5

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import express, { type Express, Request, Response } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { insertUserSchema, insertEventSchema, insertLogSchema } from "@shared/schema";
 import { z } from "zod";
 
